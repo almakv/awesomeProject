78f043f29d070b09793b01a7b742529836e96930
almakv <almakv_dev@mail.ru>
2025-04-29 11:10:48 +0300

    add middleware full loging request response

--- cmd/main.go
+++ cmd/main.go
@@ -13,8 +13,10 @@ var err error
 func main() {
 	logg = logger.LogLoad()
 	logg.SetPrefix("/cmd/main/main.go ")
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", withLoging(mainPage))
+	mux.HandleFunc("/", withFullRequestLoging(mainPage))
+
 	err = http.ListenAndServe("localhost:8080", mux)
 	if err != nil {
 		logg.Fatalln("server init error")
@@ -33,7 +35,59 @@ func withLoging(h http.HandlerFunc) http.HandlerFunc {
 		h.ServeHTTP(w, r)
 
 		duration := time.Since(start).Microseconds()
-		logg.Println("\nmethod", r.Method, "\nt: ", duration, "millisec\n", "transEnc", r.TransferEncoding)
-		logg.
+		log.Println("duration: ", duration)
+
+	}
+
+}
+
+func withFullRequestLoging(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+		log.Printf("\nr.Body : ", r.Body,
+			"\nr.ContentLength : ", r.ContentLength,
+			"\nr.Form: ", r.Form,
+			"\nr.GetBody: ", r.GetBody,
+			"\nr.Header: ", r.Header,
+			"\nr.Host: ", r.Host,
+			"\nr.Method : ", r.Method,
+			"\nr.MultipartForm : ", r.MultipartForm,
+			"\nr.Pattern : ", r.Pattern,
+			"\nr.PostForm: ", r.PostForm,
+			"\nr.Proto: ", r.Proto,
+			"\nr.ProtoMajor: ", r.ProtoMajor,
+			"\nr.ProtoMinor: ", r.ProtoMinor,
+			"\nr.RemoteAddr: ", r.RemoteAddr,
+			"\nr.RequestURI: ", r.RequestURI,
+			"\nr.Response: ", r.Response,
+			"\nr.TLS: ", r.TLS,
+			"\nr.Trailer: ", r.Trailer,
+			"\nr.Trailer: ", r.Trailer,
+			"\nr.URL: ", r.URL)
 	}
 }
+func withFullResponseLoging(h http.HandlerFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w,r)
+		
+		log.Printf(" request loging: ",
+		"\n w.Header func: ",w.Header())
+		log.Printf("\n w.Write func: ")
+		w.Write([]byte("Some slice Byte fn write"))
+		log.Println("\n w.WriteHeader func: ")
+		w.WriteHeader(1234)
+
+		log.Println("\n w.Header.Add func: ")
+		w.Header().Add()
+
+		log.Println("\n w.Header().Clone() func: ")
+		w.Header().Clone()
+
+		log.Println("\n w.Header().Del() func: ")
+		w.Header().Del(),
+
+	}
+
+
+}
+
